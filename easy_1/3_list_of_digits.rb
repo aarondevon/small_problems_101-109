790a5dfd978c0ad0b04a43c79d6fa865bca3ed49
aarondevon <me@aarondevon.com>
2017-05-05 07:11:32 -0500

    return list of digits in the number

--- easy_1/3_list_of_digits.rb
+++ easy_1/3_list_of_digits.rb
@@ -4,7 +4,7 @@ def digit_list(numbers)
   numbers.to_s.split('').map {|number| number.to_i}
 end
 
-p digit_list(12345) == [1, 2, 3, 4, 5]
+puts digit_list(12345) == [1, 2, 3, 4, 5]
 puts digit_list(7) == [7]
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0]
 puts digit_list(444) == [4, 4, 4]
