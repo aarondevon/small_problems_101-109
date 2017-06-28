dd9608c325335b885a2e536f8a64800746b16d6c
aarondevon <me@aarondevon.com>
2017-06-28 09:40:43 -0500

    prompt user for two positive numbers and perform maths

--- easy_3/02_arithmetic_integer.rb
+++ easy_3/02_arithmetic_integer.rb
@@ -2,3 +2,16 @@
 # prints the results of the following operations on those two numbers:
 # addition, subtraction, product, quotient, remainder, and power. Do not worry
 # about validating the input.
+
+puts "==> Enter the first number:"
+first_num = gets.chomp.to_i
+
+puts "==> Enter the second number:"
+second_num = gets.chomp.to_i
+
+puts "==> #{first_num} + #{second_num} = #{first_num + second_num}"
+puts "==> #{first_num} - #{second_num} = #{first_num - second_num}"
+puts "==> #{first_num} * #{second_num} = #{first_num * second_num}"
+puts "==> #{first_num} / #{second_num} = #{first_num / second_num}"
+puts "==> #{first_num} % #{second_num} = #{first_num % second_num}"
+puts "==> #{first_num} ** #{second_num} = #{first_num ** second_num}"
