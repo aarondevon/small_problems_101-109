0379fa623b291397d9c87a61098460a1d47d5a89
aarondevon <me@aarondevon.com>
2017-06-29 18:13:42 -0500

    use multiply method and create a square method

---
+++ easy_3/05_squaring_an_argument.rb
@@ -0,0 +1,11 @@
+# Using the multiply method from the "Multiplying Two Numbers" problem, write a
+# method that computes the square of its argument (the square is the result of
+# multiplying a number by itself).
+
+def multiply(num_one, num_two)
+  num_one * num_two
+end
+
+def square(num)
+  multiply(num, num)
+end
